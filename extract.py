c10ba0e34f781e176b1c513eb588893a1e154f4c
Gaz Davidson <gaz@bitplane.net>
2013-10-04 18:56:30 +0100

    Added ability to trim rows from the top and bottom of the image data
    My webcam has flashes at the top of the screen, not sure why but I'm cropping it

--- extract.py
+++ extract.py
@@ -14,13 +14,13 @@ import sys
 
 from struct import unpack
 
-def process_stream(width=320, height=240, input=sys.stdin, output=sys.stdout):
+def process_stream(width=320, height=240, input=sys.stdin, output=sys.stdout,
+                   trim_top=0, trim_bottom=0):
     """Processes a stream <todo: docstring>"""
 
-    frame_size = width*height
     threshold = 50
 
-    reader = lambda : input.read(frame_size)
+    reader = lambda : input.read(width*height)
 
     frame_number = 0
 
@@ -29,7 +29,7 @@ def process_stream(width=320, height=240, input=sys.stdin, output=sys.stdout):
         bright_value = 0
         bright_x, bright_y = (0, 0)
 
-        for y in range(height):
+        for y in range(trim_top, height-trim_bottom):
             for x in range(width):
                 pixel_value = unpack('B', frame_data[width*y + x])[0]
                 frame_total = frame_total + pixel_value
@@ -48,8 +48,7 @@ def process_stream(width=320, height=240, input=sys.stdin, output=sys.stdout):
                                                         y=bright_y)
 
         frame_number = frame_number + 1
-
-    
+   
 
 def main():
     parser = argparse.ArgumentParser(description="Observes radioactive decay in a raw video stream")
@@ -59,15 +58,20 @@ def main():
                         help='Height of the data stream in pixels')
     parser.add_argument('-i', '--input', dest='input', default=sys.stdin,
                         help='The place to get our raw data. Defaults to stdin')
-    parser.add_argument('-o', '--output',dest='output', default=sys.stdin,
+    parser.add_argument('-o', '--output', dest='output', default=sys.stdin,
                         help='A place to write the output. Defaults to stdout')
+    parser.add_argument('--trim-top', dest='tt', type=int, default=0,
+                        help='Ignore n rows at the top of each frame')
+    parser.add_argument('--trim-bottom', dest='tb', type=int, default=0,
+                        help='Ignore n rows at the bottom of each frame')
+
     #parser.add_argument('-f', '--format' ... ) # pixel format, selecting from list
 
     args = parser.parse_args()
 
     try:
         process_stream(width=args.width, height=args.height, input=args.input,
-                       output=args.output)
+                       output=args.output, trim_top=args.tt, trim_bottom=args.tb)
     finally:
         pass
         # close files 
