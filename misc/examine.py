e7576b6271751f856c6a3781feabe5ca6c4632b7
Gaz Davidson <gaz@bitplane.net>
2013-11-01 18:08:10 +0000

    Forgot this file, changed for CSV format change

--- misc/examine.py
+++ misc/examine.py
@@ -19,13 +19,10 @@ def main():
     with open(filename) as fin:
         for line in fin:
             (frame, brightness, x, y) = [int(s) for s in line[:-1].split(',')]
-            if lastframe < frame:
-                frame_diffs.append(frame - lastframe)
-                x_positions.append(x)
-                y_positions.append(y)
-                brightnesses.append(brightness)
-
-            lastframe = frame
+            frame_diffs.append(frame)
+            x_positions.append(x)
+            y_positions.append(y)
+            brightnesses.append(brightness)
 
     # plot some stuff...
     plt.figure(figsize=(10,7), dpi=100)
